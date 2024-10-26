edd5e6ffa34cdd6970684d4b76dfc7ab8666825a
yanshuman <yanshuman2005@gmail.com>
2024-10-26 15:18:34 +0530

    Update react-native-voice version in package.json and package-lock.json

--- app/(drawers)/(tabs)/_layout.tsx
+++ app/(drawers)/(tabs)/_layout.tsx
@@ -47,19 +47,20 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="recordings"
+        name="chatbot"
         options={{
           headerShown: false,
-          title: "Recordings",
+          title: "Shakti",
           tabBarIcon: ({ color }) => (
             <MaterialCommunityIcons
-              name="record-circle"
-              size={26}
+              name="face-woman-outline"
+              size={28}
               color={color}
             />
           ),
         }}
       />
+
       <Tabs.Screen
         name="here"
         options={{
@@ -74,16 +75,7 @@ export default function TabLayout() {
           ),
         }}
       />
-      <Tabs.Screen
-        name="history"
-        options={{
-          headerShown: false,
-          title: "History",
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="history" size={28} color={color} />
-          ),
-        }}
-      />
+
       <Tabs.Screen
         name="myfamily"
         options={{
